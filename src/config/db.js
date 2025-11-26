e420faa405433bac1daa31e9ecb2db23600b3c3c
Liminhaowo <matheus.lima0w0@gmail.com>
2025-11-26 12:21:40 -0300

    puxando a main pro LIMA ebaaa

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,7 @@ import mysql from "mysql2/promise";
 export const db = await mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "senai",
+  password: "senai2025",
   database: "dblivraria",
   port: 3306,
 });
