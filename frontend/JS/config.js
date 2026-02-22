2078bd25007005dc40bcde47edc2ca7cd8f81a16
Matheus <alvesdemelomatheus4@gmail.com>
2026-02-22 16:15:22 -0300

    Ota coisa

--- frontend/JS/config.js
+++ frontend/JS/config.js
@@ -5,7 +5,7 @@
 // Em produção: troque pelo domínio do seu servidor (ex: https://api.meusite.com)
 import "dotenv/config";
 
-const API_BASE_URL = process.env.CLIENT_URL || "http://localhost:3000";
+const API_BASE_URL = process.env.CLIENT_URL + "/api" || "http://localhost:3000";
 
 
 export default API_BASE_URL;
